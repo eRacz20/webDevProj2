79c56db1c7951a35d7bd5dcf74d31295146656ef
eRacz20 <evanracz20@gmail.com>
2026-02-22 19:55:11 -0500

    Fix hopefully

--- backend/app.py
+++ backend/app.py
@@ -101,5 +101,7 @@ def delete_movie(id):
 
     return {"message": "Deleted"}
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
