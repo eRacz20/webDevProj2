31b9fb757815bdb00acd0001fd744016fe83bca1
eRacz20 <evanracz20@gmail.com>
2026-02-22 20:27:38 -0500

    small change

--- backend/app.py
+++ backend/app.py
@@ -87,4 +87,5 @@ def stats():
     """)
     return jsonify(cur.fetchone())
 
-app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
